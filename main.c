ab5e4b1f47232595462724ca949b80f21f534c0a
Jaehun Kim <wogns3623@gmail.com>
2020-05-08 14:32:52 +0000

    Add line splitting function

--- main.c
+++ main.c
@@ -5,12 +5,35 @@
 #include <unistd.h>
 #include <string.h>
 
+#define BUF_SIZE 1024
+#define MAX_COMMAND_LENGTH 4096
 
-#define BUF_SIZE 100 /* The maximum length command */
+int getcom(char *command, int command_length, int buf_size) {
+  int eoc = 1;
+  char buf[buf_size];
+  command[0] = '\0';
+
+  do {
+    fgets(buf, buf_size, stdin);
+    if (buf[strlen(buf)-1] == '\n')
+      buf[strlen(buf)-1] = '\0';
+
+    if (buf[strlen(buf)-1] == '\\') {
+      buf[strlen(buf)-1] = '\0';
+    } else {
+      eoc = 0;
+    }
+    strcat(command, buf);
+  } while (eoc);
+
+  return 0;
+}
+
+int parse();
 
 int main(int argc, char *argv[]) {
   int run = 1, eoc = 1;
-  char *buf = (char *)malloc(BUF_SIZE*sizeof(char));
+  char *command = (char *)malloc(MAX_COMMAND_LENGTH*sizeof(char));
 
   if (argc > 1) {
     //execute input command
@@ -19,9 +42,8 @@ int main(int argc, char *argv[]) {
     while (run) {
       fputs("> ", stdout);
       fflush(stdout);
-      fgets(buf, BUF_SIZE, stdin);
-      buf[strlen(buf)-1] = '\0';
-      system(buf);
+      getcom(command, MAX_COMMAND_LENGTH, BUF_SIZE);
+      system(command);
     }
   }
 
